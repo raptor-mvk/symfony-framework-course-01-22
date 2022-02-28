30b51e279a32182167bbc4510dd948ff5eac1bce
Mikhail Kamorin <m.v.kamorin@gmail.com>
2022-02-28 17:18:43 +0300

    Add ExceptionListener

--- src/Controller/Api/v1/UserController.php
+++ src/Controller/Api/v1/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Api\v1;
 
 use App\Entity\User;
+use App\Exception\DeprecatedApiException;
 use App\Manager\UserManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -23,6 +24,8 @@ class UserController extends AbstractController
     #[Route(path: '', methods: ['POST'])]
     public function saveUserAction(Request $request): Response
     {
+        throw new DeprecatedApiException('This API method is deprecated');
+
         $login = $request->request->get('login');
         $userId = $this->userManager->saveUser($login);
         [$data, $code] = $userId === null ?
