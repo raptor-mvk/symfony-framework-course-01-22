e490572f4c66e0686b9613bb009cc1e7aa228215
Mikhail Kamorin <m.v.kamorin@gmail.com>
2022-03-08 19:24:29 +0300

    Fix error

--- src/Security/AppLoginAuthenticator.php
+++ src/Security/AppLoginAuthenticator.php
@@ -49,9 +49,7 @@ class AppLoginAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('app_api_v3_user_getusers'));
     }
 
     protected function getLoginUrl(Request $request): string
