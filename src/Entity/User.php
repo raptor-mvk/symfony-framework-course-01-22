1922749edd94dff2309bcf3daaa01806739bef8d
Mikhail Kamorin <m.v.kamorin@gmail.com>
2022-03-08 22:54:40 +0300

    Add attributes for serialized types

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
+use JMS\Serializer\Annotation as JMS;
 use JsonException;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -55,9 +56,11 @@ class User implements HasMetaTimestampsInterface, UserInterface, PasswordAuthent
     private string $password;
 
     #[ORM\Column(type: 'integer', nullable: false)]
+    #[JMS\Type('string')]
     private int $age;
 
     #[ORM\Column(type: 'boolean', nullable: false)]
+    #[JMS\Type('int')]
     private bool $isActive;
 
     #[ORM\Column(type: 'string', length: 1024, nullable: false)]
