80a4966d938f5319eefefb740eb189ab6e421a86
Mikhail Kamorin <m.v.kamorin@gmail.com>
2022-03-18 18:19:26 +0300

    Add monolog & log some messages

--- src/Controller/Api/SaveUser/v5/SaveUserManager.php
+++ src/Controller/Api/SaveUser/v5/SaveUserManager.php
@@ -8,6 +8,7 @@ use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
+use Psr\Log\LoggerInterface;
 
 class SaveUserManager
 {
@@ -15,14 +16,25 @@ class SaveUserManager
 
     private SerializerInterface $serializer;
 
-    public function __construct(EntityManagerInterface $entityManager, SerializerInterface $serializer)
+    private LoggerInterface $logger;
+
+    public function __construct(EntityManagerInterface $entityManager, SerializerInterface $serializer, LoggerInterface $logger)
     {
         $this->entityManager = $entityManager;
         $this->serializer = $serializer;
+        $this->logger = $logger;
     }
 
     public function saveUser(SaveUserDTO $saveUserDTO): UserIsSavedDTO
     {
+        $this->logger->debug('This is debug message');
+        $this->logger->info('This is info message');
+        $this->logger->notice('This is notice message');
+        $this->logger->warning('This is warning message');
+        $this->logger->error('This is error message');
+        $this->logger->critical('This is critical message');
+        $this->logger->alert('This is alert message');
+        $this->logger->emergency('This is emergency message');
         $user = new User();
         $user->setLogin($saveUserDTO->login);
         $user->setPassword($saveUserDTO->password);
