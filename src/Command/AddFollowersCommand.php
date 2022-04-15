fe33bd22849cf224e40d73d8097a294e4a54d0ca
Mikhail Kamorin <m.v.kamorin@gmail.com>
2022-04-15 17:19:52 +0300

    Block parallel command runs

--- src/Command/AddFollowersCommand.php
+++ src/Command/AddFollowersCommand.php
@@ -5,6 +5,7 @@ namespace App\Command;
 use App\Service\SubscriptionService;
 use App\Manager\UserManager;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Command\LockableTrait;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -12,6 +13,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class AddFollowersCommand extends Command
 {
+    use LockableTrait;
+
     private const DEFAULT_FOLLOWERS = 100;
     private const DEFAULT_LOGIN_PREFIX = 'Reader #';
 
@@ -38,6 +41,11 @@ class AddFollowersCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        if (!$this->lock()) {
+            $output->writeln('<info>Command is already running.</info>');
+            return self::SUCCESS;
+        }
+        sleep(100);
         $authorId = (int)$input->getArgument('authorId');
         $user = $this->userManager->findUserById($authorId);
         if ($user === null) {
