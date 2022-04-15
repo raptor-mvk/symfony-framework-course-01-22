78631342d817bf2ee41ea9c114b8c9cc284b85fb
Mikhail Kamorin <m.v.kamorin@gmail.com>
2022-04-15 17:34:31 +0300

    Add interactive argument

--- src/Command/AddFollowersCommand.php
+++ src/Command/AddFollowersCommand.php
@@ -13,6 +13,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
 
 class AddFollowersCommand extends Command
 {
@@ -40,7 +41,6 @@ class AddFollowersCommand extends Command
             ->setHidden(true)
             ->setDescription('Adds followers to author')
             ->addArgument('authorId', InputArgument::REQUIRED, 'ID of author')
-            ->addArgument('count', InputArgument::OPTIONAL, 'How many followers should be added')
             ->addOption('login', 'l', InputOption::VALUE_REQUIRED, 'Follower login prefix');
     }
 
@@ -52,7 +52,9 @@ class AddFollowersCommand extends Command
             $output->write("<error>User with ID $authorId doesn't exist</error>\n");
             return self::FAILURE;
         }
-        $count = (int)($input->getArgument('count') ?? self::DEFAULT_FOLLOWERS);
+        $helper = $this->getHelper('question');
+        $question = new Question('How many followers you want to add?', self::DEFAULT_FOLLOWERS);
+        $count = (int)$helper->ask($input, $output, $question);
         if ($count < 0) {
             $output->write("<error>Count should be positive integer</error>\n");
             return self::FAILURE;
--- tests/unit/Command/AddFollowersCommandTest.php
+++ tests/unit/Command/AddFollowersCommandTest.php
@@ -54,12 +54,11 @@ class AddFollowersCommandTest extends FixturedTestCase
         $userManager = new UserManager($this->getDoctrineManager(), $formFactory, $encoder, $finder);
         $author = $userManager->findUserByLogin(MultipleUsersFixture::PRATCHETT);
         $params = ['authorId' => $author->getId()];
-        $options = ['login' => $login];
-        if ($followersCount !== null) {
-            $params['count'] = $followersCount;
-        }
-        $commandTester->execute($params, $options);
+        $inputs = $followersCount === null ? ["\n"] : ["$followersCount\n"];
+        $commandTester->setInputs($inputs);
+        $commandTester->execute($params);
         $output = $commandTester->getDisplay();
+
         static::assertSame($expected, $output);
     }
 }
