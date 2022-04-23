90084ecfc67080a89fa12aa22098a8d238247d2d
Mikhail Kamorin <m.v.kamorin@gmail.com>
2022-04-23 18:48:41 +0300

    Add config to bundle

--- src/StatsdBundle/DependencyInjection/StatsdExtension.php
+++ src/StatsdBundle/DependencyInjection/StatsdExtension.php
@@ -2,6 +2,7 @@
 
 namespace StatsdBundle\DependencyInjection;
 
+use StatsdBundle\Client\StatsdAPIClient;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
@@ -13,5 +14,13 @@ class StatsdExtension extends Extension
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yaml');
+
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $serviceDefinition = $container->getDefinition(StatsdAPIClient::class);
+        $serviceDefinition->replaceArgument(0, $config['client']['host']);
+        $serviceDefinition->replaceArgument(1, $config['client']['port']);
+        $serviceDefinition->replaceArgument(2, $config['client']['namespace']);
     }
 }
