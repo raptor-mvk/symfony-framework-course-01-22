436de614d89658dd1fa3ca1f9018f999cfa78104
Mikhail Kamorin <m.v.kamorin@gmail.com>
2022-05-13 14:45:23 +0300

    Fix message serialization

--- src/DTO/AddFollowersDTO.php
+++ src/DTO/AddFollowersDTO.php
@@ -3,14 +3,30 @@
 namespace App\DTO;
 
 use JsonException;
+use JMS\Serializer\Annotation as JMS;
 
 class AddFollowersDTO
 {
+    #[JMS\Exclude]
     private array $payload;
+
+    #[JMS\Type('int')]
+    #[JMS\SerializedName('userId')]
+    private int $userId;
+
+    #[JMS\Type('string')]
+    #[JMS\SerializedName('followerLogin')]
+    private string $followersLogin;
+
+    #[JMS\Type('int')]
+    private int $count;
 
     public function __construct(int $userId, string $followerLogin, int $count)
     {
         $this->payload = ['userId' => $userId, 'followerLogin' => $followerLogin, 'count' => $count];
+        $this->userId = $userId;
+        $this->followersLogin = $followerLogin;
+        $this->count = $count;
     }
 
     /**
